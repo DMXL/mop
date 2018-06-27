c83b4893a3588d7f1f6ee0cd9d791e1af9ece6f9
kshi <keshi@qiyi.com>
2018-06-27 18:53:43 +0800

    Update homebrew .rb file

--- homebrew/mop.rb
+++ homebrew/mop.rb
@@ -1,16 +1,16 @@
 require "formula"
 
 class Mop < Formula
-  homepage "https://github.com/michaeldv/mop"
-  head     "https://github.com/michaeldv/mop.git"
-  url      "https://github.com/michaeldv/mop/archive/v0.1.0.tar.gz"
-  sha1     "b3bd5b529430da22bfa1beeca435a25e72513e27"
+  homepage "https://github.com/mop-tracker/mop" 
+  head     "https://github.com/mop-tracker/mop.git" 
+  url      "https://codeload.github.com/mop-tracker/mop/tar.gz/v0.2.1" 
+  sha256   "cf11ce0967fc02aa6ed7852866973aeb66d2e5729ca1fe7997d9d58d64d8e04a" 
 
   depends_on "go" => :build
 
   def install
     system "go", "get", "github.com/michaeldv/termbox-go"
-    system "go build cmd/mop.go"
+    system "go build cmd/main.go"
     bin.install "mop"
   end
 end
