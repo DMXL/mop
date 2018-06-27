0c4f52cd444e99da0aecfbb555205cb1207c95a4
dmon <dm@dmon-studio.com>
2018-06-27 19:11:20 +0800

    Update brew .rb file

--- homebrew/mop.rb
+++ homebrew/mop.rb
@@ -1,16 +1,16 @@
 require "formula"
 
 class Mop < Formula
-  homepage "https://github.com/mop-tracker/mop" 
-  head     "https://github.com/mop-tracker/mop.git" 
-  url      "https://codeload.github.com/mop-tracker/mop/tar.gz/v0.2.1" 
-  sha256   "cf11ce0967fc02aa6ed7852866973aeb66d2e5729ca1fe7997d9d58d64d8e04a" 
+  homepage "https://github.com/DMXL/mop" 
+  head     "https://github.com/DMXL/mop.git" 
+  url      "https://codeload.github.com/DMXL/mop/tar.gz/v0.2.2" 
+  sha256   "5fa4d52dea3f51468dfaff3556af933096aeffac6f972f3f3e8182edbd5d04a9" 
 
   depends_on "go" => :build
 
   def install
     system "go", "get", "github.com/michaeldv/termbox-go"
-    system "go build cmd/main.go"
+    system "go build cmd/mop/main.go"
     bin.install "mop"
   end
 end
